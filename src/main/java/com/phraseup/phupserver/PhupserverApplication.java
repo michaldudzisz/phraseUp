13057217fd414988addde89b5e1f0c86f369e1b1
michaldudzisz <michaldudzisz@gmail.com>
2020-10-10 22:52:22 +0200

    Starting from zero 2

---
+++ src/main/java/com/phraseup/phupserver/PhupserverApplication.java
@@ -0,0 +1,13 @@
+package com.phraseup.phupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhupserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhupserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phraseup/phupserver/PhupserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phraseup.phupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhupserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
